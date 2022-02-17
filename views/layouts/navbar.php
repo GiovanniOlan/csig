c3b371663fdffe773341f6c311ec241e69f50007
Gio845 <yovi8456@gmail.com>
2022-02-17 09:51:05 -0600

    User duenio

--- views/layouts/navbar.php
+++ views/layouts/navbar.php
@@ -3,6 +3,7 @@
 use yii\helpers\Html;
 use yii\bootstrap4\Nav;
 use yii\bootstrap4\NavBar;
+use webvimark\modules\UserManagement\UserManagementModule;
 
 
 
@@ -23,11 +24,21 @@ use yii\bootstrap4\NavBar;
     $menuItems[] = ['label' => 'Nosotros', 'url' => '/site/nosotros'];
     $menuItems[] = ['label' => 'Contáctanos', 'url' => '/contacto'];
     $menuItems[] = ['label' => 'Productos', 'url' => '/site/productos'];
+    $menuItems[] = [
+        'label' => 'Administrador',
+        'items' => UserManagementModule::menuItems(),
+        'options' => [
+            'class' => '  list-inline-item last-item '
+        ],
+        'encodeLabels' => false,
+        'visible' => Yii::$app->user->isSuperAdmin,
+    ];
     $menuItems[] = ['label' => 'CERRAR SESIÓN', 'url' => '/user-management/auth/logout', 'visible' => Yii::$app->user->isSuperAdmin, 'options' => ['style' => 'font-family: fangsong;']];
     ?>
     <?= Nav::widget([
         'options' => ['class' => 'navbar-nav ms-auto mb-2 mb-lg-0'],
-        'items' => $menuItems
+        'items' => $menuItems,
+        'encodeLabels' => false,
     ]);
     NavBar::end();
     ?>
